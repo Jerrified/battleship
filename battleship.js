de01310e5f4f111d424be79d6a8a646b5c78a7dc
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-08-04 14:49:06 -0400

    More modifications

--- battleship.js
+++ battleship.js
@@ -1,9 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var random_number = Math.floor(Math.random() * 6);
 var guess;
-var hits = 0;
 var misses = 0;
+var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
@@ -16,15 +14,12 @@ while (isSunk == false){
         guesses = guesses + 1; 
         }
 
-    if (guess == location1 || guess == location2 || guess == location3) { 
+    if (guess == random_number) { 
             alert("You hit enemy battleship!");
             hits = hits + 1; 
                 if (hits == 3) {
                     isSunk = true;
                     alert("You sank my battleship!"); 
-                }
-                if (misses == 3) {
-                    alert("Your battleship sank!")
                 }
     }   else {
             alert("You missed!");
@@ -33,5 +28,5 @@ while (isSunk == false){
 }
 
     var stats = "You took " + guesses + " guesses to sink the battleship, " +
-    "which means your shooting accuracy was " + (3/guesses); 
+    "and you missed " + misses + " times, which means your shooting accuracy was " + (3/guesses); 
     alert(stats);
