960ce413c3f70017ca79335519bf9eb8be3a65f9
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-08-04 13:27:44 -0400

    Modified battleship game

--- battleship.js
+++ battleship.js
@@ -3,11 +3,12 @@ var location2 = 4;
 var location3 = 5;
 var guess;
 var hits = 0;
+var misses = 0;
 var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false){
-    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):")
 
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
@@ -16,19 +17,20 @@ while (isSunk == false){
         }
 
     if (guess == location1 || guess == location2 || guess == location3) { 
-            alert("HIT!");
+            alert("You hit enemy battleship!");
             hits = hits + 1; 
                 if (hits == 3) {
                     isSunk = true;
                     alert("You sank my battleship!"); 
                 }
+                if (misses == 3) {
+                    alert("Your battleship sank!")
+                }
     }   else {
-            alert("MISS");
+            alert("You missed!");
+            misses = misses + 1;
          }
-         if (isSunk == true) {
-             
-         }
-    }
+}
 
     var stats = "You took " + guesses + " guesses to sink the battleship, " +
     "which means your shooting accuracy was " + (3/guesses); 
