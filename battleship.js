b6b38bd4503489b4f4bf6ba5bc612244c3105099
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-08-05 12:55:10 -0400

    Modifications

--- battleship.js
+++ battleship.js
@@ -1,38 +1,50 @@
-var random_number = Math.floor(Math.random() * 6);
+var location = Math.floor(Math.random() * 7);
 var guess;
-var misses = 0;
 var hits = 0;
+var miss = 0;
 var guesses = 0;
-var isSunk = false;
+var isSunk = null;
+var answer1 = "Yes" || "yes";
+var answer2 = "No" || "no";
 
-
-while (isSunk == false){
+while (isSunk == null){
     guess = prompt("Ready, aim, fire! (enter a number from 0-6):")
 
-    if (guess < 0 || guess > 6) {
+    if (guess < 0 || guess > 6 || guess == null) {
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1; 
         }
 
-    if (guess == random_number) { 
-            alert("You hit enemy battleship!");
+    if (guess == location) { 
+            alert("HIT!");
             hits = hits + 1; 
                 if (hits == 3) {
                     isSunk = true;
                     alert("You sank my battleship!"); 
                 }
     }   else {
-            alert("You missed!");
-            misses = misses + 1;
-         }
-        if (isSunk == true) {
-            prompt("Do you want to play again? (Type Yes or No)")
-            Option(No)
-
+            alert("MISS");
+            miss = miss + 1;
+            if (miss == 3) {
+                isSunk = false;
+                alert("Got you!");
+                while (isSunk == false) {
+                    prompt("I got you! HAHHAHAA! Play again? (Enter Yes or No):")
+                    if (answer1) {
+                        isSunk = null;
+                        guesses = 0;
+                        hits = 0;
+                        miss = 0;
+                        if (answer2) {
+                            alert("You are now finished playing battleship!")
+                        }
+                    }
+                }
         }
+    }
 }
 
     var stats = "You took " + guesses + " guesses to sink the battleship, " +
-    "and you missed " + misses + " times, which means your shooting accuracy was " + (3/guesses); 
+    "which means your shooting accuracy was " + (3/guesses); 
     alert(stats);
